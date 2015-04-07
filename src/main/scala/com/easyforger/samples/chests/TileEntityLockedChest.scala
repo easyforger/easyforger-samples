18bb0988fff2c497fdd0b0bda4d8bb409e5f1645
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-04-06 23:41:11 -0300

    first commit of the sample mods after being extracted from the core

---
+++ src/main/scala/com/easyforger/samples/chests/TileEntityLockedChest.scala
@@ -0,0 +1,12 @@
+package com.easyforger.samples.chests
+
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.tileentity.TileEntityChest
+
+class TileEntityLockedChest extends TileEntityChest {
+  func_145976_a("locked chest")
+
+  override def isUseableByPlayer(player: EntityPlayer): Boolean = {
+    player.getHeldItem != null && player.getHeldItem.getUnlocalizedName.contains("chestkey")
+  }
+}
---
+++ src/main/scala/com/easyforger/samples/items/ItemChestKey.scala
@@ -0,0 +1,8 @@
+package com.easyforger.samples.items
+
+import com.easyforger.items.EasyForgerItem
+import net.minecraft.creativetab.CreativeTabs
+
+class ItemChestKey extends EasyForgerItem(ItemsMod.modId, "chestkey", List("yellow", "red", "blue")) {
+  setCreativeTab(CreativeTabs.tabMisc)
+}
