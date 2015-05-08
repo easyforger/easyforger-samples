445bb324ea7a303059811d98dba917f1a22e7fb4
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-05-08 12:18:16 -0300

    update BlocksMod to minecraft / forge / fml 1.8

--- src/main/scala/com/easyforger/samples/blocks/BlocksMod.scala
+++ src/main/scala/com/easyforger/samples/blocks/BlocksMod.scala
@@ -1,21 +1,20 @@
 package com.easyforger.samples.blocks
 
 import com.easyforger.recipes._
-import cpw.mods.fml.common.Mod
-import cpw.mods.fml.common.Mod.EventHandler
-import cpw.mods.fml.common.event.FMLPreInitializationEvent
-import cpw.mods.fml.common.registry.GameRegistry
 import net.minecraft.init.Items._
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
 
-@Mod(modid = BlocksMod.modId, name = "EasyForger Blocks Sample Mod", version = "0.1", modLanguage = "scala")
+@Mod(modid = BlocksMod.modId, name = "EasyForger Blocks Sample Mod", version = "0.2", modLanguage = "scala")
 object BlocksMod {
   final val modId = "easyforger_blocks"
 
   val cloth = new BlockCloth()
 
   @EventHandler
-  def preInit(event: FMLPreInitializationEvent) = {
-    GameRegistry.registerBlock(cloth, "BlockCloth")
+  def init(event: FMLInitializationEvent) = {
+    cloth.register()
 
     crafting(
       string to cloth withShape
