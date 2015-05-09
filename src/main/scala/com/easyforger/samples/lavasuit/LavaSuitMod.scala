3bd55f023fe00da8aa63d3e2b4c70454d4c74e69
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-05-09 16:21:15 -0300

    (easyforger #20) update LavaSuitMod to minecraft / forge / fml 1.8

--- src/main/scala/com/easyforger/samples/lavasuit/LavaSuitMod.scala
+++ src/main/scala/com/easyforger/samples/lavasuit/LavaSuitMod.scala
@@ -2,18 +2,17 @@ package com.easyforger.samples.lavasuit
 
 import com.easyforger.items.{Boots, ChestPlate, Helmet, Leggings}
 import com.easyforger.recipes._
-import cpw.mods.fml.common.Mod
-import cpw.mods.fml.common.Mod.EventHandler
-import cpw.mods.fml.common.event.FMLPreInitializationEvent
-import cpw.mods.fml.common.registry.GameRegistry
 import net.minecraft.init.Items
 import net.minecraftforge.common.util.EnumHelper
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
 
-@Mod(modid = LavaSuitMod.modId, name = "EasyForger Armor LavaSuit Mod", version = "0.1", modLanguage = "scala")
+@Mod(modid = LavaSuitMod.modId, name = "EasyForger Armor LavaSuit Mod", version = "0.2", modLanguage = "scala")
 object LavaSuitMod {
   final val modId = "easyforger_lavasuit"
 
-  val lavaMaterial = EnumHelper.addArmorMaterial("lavasuit", 20, Array(4, 9, 7, 4), 10)
+  val lavaMaterial = EnumHelper.addArmorMaterial("lavasuit", "lavasuit", 20, Array(4, 9, 7, 4), 10)
 
   val lavaHelmet = new LavaSuitItemArmor(lavaMaterial, Helmet)
   val lavaChestPlate = new LavaSuitItemArmor(lavaMaterial, ChestPlate)
@@ -21,11 +20,11 @@ object LavaSuitMod {
   val lavaBoots = new LavaSuitItemArmor(lavaMaterial, Boots)
 
   @EventHandler
-  def preInit(event: FMLPreInitializationEvent) = {
-    GameRegistry.registerItem(lavaHelmet, "LavaSuitHelmet")
-    GameRegistry.registerItem(lavaChestPlate, "LavaSuitChest")
-    GameRegistry.registerItem(lavaLeggings, "LavaSuitLeggings")
-    GameRegistry.registerItem(lavaBoots, "LavaSuitBoots")
+  def init(event: FMLInitializationEvent) = {
+    lavaHelmet.register()
+    lavaChestPlate.register()
+    lavaLeggings.register()
+    lavaBoots.register()
 
     crafting(
       Items.iron_ingot + Items.lava_bucket to lavaHelmet withShape
