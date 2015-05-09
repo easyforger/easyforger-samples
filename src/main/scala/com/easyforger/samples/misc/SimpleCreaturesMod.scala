c0a3896ed475b714b9ca89682994819e840db9a0
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-05-09 11:45:16 -0300

    (easyforger #20) update SimpleCreaturesMod to minecraft / forge / fml 1.8

--- src/main/scala/com/easyforger/samples/misc/SimpleCreaturesMod.scala
+++ src/main/scala/com/easyforger/samples/misc/SimpleCreaturesMod.scala
@@ -2,12 +2,12 @@ package com.easyforger.samples.misc
 
 import com.easyforger.base.EasyForger
 import com.easyforger.creatures._
-import cpw.mods.fml.common.Mod
-import cpw.mods.fml.common.Mod.EventHandler
-import cpw.mods.fml.common.event.FMLPreInitializationEvent
 import net.minecraft.init.Items._
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 
-@Mod(modid = "easyforger_creatures_simple", name = "EasyForger Vanilla Creatures Replacements", version = "0.1", modLanguage = "scala")
+@Mod(modid = "easyforger_creatures_simple", name = "EasyForger Vanilla Creatures Replacements", version = "0.2", modLanguage = "scala")
 object SimpleCreaturesMod extends EasyForger {
 
   @EventHandler
