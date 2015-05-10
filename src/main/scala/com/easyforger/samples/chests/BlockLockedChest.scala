58c73f0fcdee5ee35921d709ca40ed3fc5178c78
Paulo 'JCranky' Siqueira <paulo.siqueira@gmail.com>
2015-05-10 00:09:56 -0300

    (easyforger #24) specify the type of chest being extended

--- src/main/scala/com/easyforger/samples/chests/BlockLockedChest.scala
+++ src/main/scala/com/easyforger/samples/chests/BlockLockedChest.scala
@@ -1,9 +1,9 @@
 package com.easyforger.samples.chests
 
-import com.easyforger.blocks.EFBlockChest
+import com.easyforger.blocks.{ChestType, EFBlockChest}
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.world.World
 
-class BlockLockedChest extends EFBlockChest(LockedChestsMod.modId, "lockedchest") {
+class BlockLockedChest extends EFBlockChest(LockedChestsMod.modId, "lockedchest", ChestType.Normal) {
   override def createNewTileEntity(worldIn: World, meta: Int): TileEntity = new TileEntityLockedChest()
 }
