7c8f66b5c32c6c08185a74cb7fda1de8398185a8
JCranky <paulo.siqueira@gmail.com>
2017-01-01 20:54:46 +0100

    Change EFItemFood.addPotionEffect to take a PotionEffect

--- src/main/scala/com/easyforger/samples/items/ItemBanana.scala
+++ src/main/scala/com/easyforger/samples/items/ItemBanana.scala
@@ -10,5 +10,5 @@ import net.minecraft.potion.PotionEffect
 
 class ItemBanana extends EFItemFood(ItemsMod.modId, "banana", 5, 0.4f, false) {
   setPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 9 * 20, 1), 1f)
-  addPotionEffect(MobEffects.MINING_FATIGUE, 5, 0, 0.5f)
+  addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, 5 * 20, 0), 0.5f)
 }
